88e6ee3cda1b661ea332b0ec02b40ec8e2be6572
Vyacheslav Tyutyunkov <ve@tyutyunkov.net>
2023-12-09 22:40:19 +0100

    day 9

---
+++ 2023/advent_09.js
@@ -0,0 +1,31 @@
+const parse = input => input
+    .map(str => str.split(' ').map(v => v - 0))
+
+const diffRow = row => {
+    return row.slice(1).reduce(({c, diff}, v) => ({
+            c: v,
+            diff: [...diff, v - c]
+        }),
+        {c: row[0], diff: []}
+    ).diff
+}
+
+const buildDiffs = row => {
+    const diffs = [row];
+    let diff = row;
+    do {
+        diff = diffRow(diff);
+        diffs.push(diff)
+    } while (diff.some(v => v !== 0))
+    return diffs;
+}
+
+exports.part1 = input =>
+    parse(input).map(buildDiffs)
+        .map(diffs => diffs.reduceRight((acc, r) => acc + r[r.length - 1], 0))
+        .reduce((acc, v) => acc + v, 0);
+
+exports.part2 = input =>
+    parse(input).map(buildDiffs)
+        .map(diffs => diffs.reduceRight((acc, r) => r[0] - acc, 0))
+        .reduce((acc, v) => acc + v, 0);
