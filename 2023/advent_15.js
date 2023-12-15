056b119bbdc3bc15dc5eb1b347a82bb01b5f7d39
Vyacheslav Tyutyunkov <ve@tyutyunkov.net>
2023-12-15 06:32:15 +0100

    day 15

---
+++ 2023/advent_15.js
@@ -0,0 +1,28 @@
+const parse = input => input[0].split(',').map(str => str.trim())
+
+const hash = str => str.split('')
+    .map(c => c.charCodeAt(0))
+    .reduce((acc, c) => (acc + c) * 17 % 256, 0);
+
+exports.part1 = input => parse(input).map(hash).reduce((acc, v) => acc + v, 0);
+
+exports.part2 = input =>
+    Object.entries(
+        parse(input)
+            .map(str => str.endsWith('-')
+                ? ({s: str.substring(0, str.length - 1), op: '-'})
+                : ({s: str.split('=')[0], op: '=', len: str.split('=')[1]})
+            )
+            .reduce((acc, {s, op, len}) => {
+                const hashValue = hash(s);
+                const box = acc[hashValue] ?? [];
+                return op === '-'
+                    ? ({...acc, [hashValue]: box.filter(v => v.s !== s)})
+                    : (!box.some(v => v.s === s)
+                            ? ({...acc, [hashValue]: [...box, {s, len}]})
+                            : ({...acc, [hashValue]: box.map(v => v.s === s ? {s, len} : v)})
+                    );
+            }, {})
+    )
+        .flatMap(([i, box]) => box.map(({len}, j) => (i - 0 + 1) * (j + 1) * len))
+        .reduce((acc, v) => acc + v, 0);
